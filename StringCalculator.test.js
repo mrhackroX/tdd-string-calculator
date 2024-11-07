79b4898c81569d4c811f832553ef65dad3603e71
Shubham Maurya <shubhammaurya@Shubhams-MacBook-Air.local>
2024-11-07 18:02:17 +0530

    add two comma seperated values. This will work for any numbers of values seperated by comma

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,7 +1,7 @@
 // add two comma seperated values. This will work for any numbers of values seperated by comma.
 const { add } = require("./StringCalculator");
 
-test("If string has only one value, return that value", () => {
+test("add two comma seperated values", () => {
   expect(add("23,17")).toBe(40);
   expect(add("23,17,45")).toBe(85);
 });
